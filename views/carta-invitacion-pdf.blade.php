939007980241b64a58ae090682af571485efc436
alvarogarcia1010 <00111317@uca.edu.sv>
2019-05-10 16:41:53 -0600

    Issue #1

--- views/carta-invitacion-pdf.blade.php
+++ views/carta-invitacion-pdf.blade.php
@@ -98,14 +98,14 @@
           <tr>
             <td>
               <div class="col text-center">
-                <img height="75px"src="https://storage.googleapis.com/decimaerp-cloud-bucket/organizations/15/Firma_ARS.jpg">
+                <img height="75px"src="https://storage.googleapis.com/decimaerp-cloud-bucket/organizations/15/Firma_ARS_v2.png">
                 <p style="line-height: 2pt;margin-bottom: 0;">Axel Ruiz Serrano</p>
                 <p style="font-size: 0.8rem;line-height: 10pt;">Representante General del<br>Comité Organizador del ECSL</p>
               </div>
             </td>
             <td>
               <div class="col text-center">
-                <img height="75px" src="https://storage.googleapis.com/decimaerp-cloud-bucket/organizations/15/firma_MCCM.png">
+                <img height="100px" src="https://storage.googleapis.com/decimaerp-cloud-bucket/organizations/15/firma_MCCM.png">
                 <p style="line-height: 2pt;margin-bottom: 0;">Maria del Carmen Castillo Martini</p>
                 <p style="font-size: 0.8rem;line-height: 10pt;">Administración y Marketing del<br>Comité Organizador del ECSL</p>
               </div>
