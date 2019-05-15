fc3ebcd628259ed7489c915c4ca0be1a42640503
alvarogarcia1010 <00111317@uca.edu.sv>
2019-05-15 11:25:14 -0600

    Issue #1

--- views/paquetes-participacion.blade.php
+++ views/paquetes-participacion.blade.php
@@ -1,34 +1,39 @@
+<style media="screen">
+  .card-deck{
+    font-size: 0.80em;
+  }
+</style>
 <div class="card-deck">
 
   <!-- Paquete Básico -->
   <div class="card" data-type="A" data-payment-type-id="190" data-payment-commission-amount="0" data-amount="10" data-amount-cr="6000" data-description="Paquete básico ECSL 2019">
-    <h3 class="card-header text-center">Básico</h3>
+    <h5 class="card-header text-center">Básico</h5>
     <div class="card-body m-0 px-0 pb-0">
-      <h2 class="text-center font-weight-bold">US$ 10.00</h2>
+      <h3 class="text-center font-weight-bold">US$ 10.00</h3>
       <div class="font-italic text-center mb-4">Incluye:</div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">* Ingreso al evento <i class="fa fa-check-circle-o icon-check"></i></li>
         <li class="list-group-item">Promocional básico <i class="fa fa-check-circle-o icon-check"></i></li>
         <li class="list-group-item">Diploma de participación <i class="fa fa-check-circle-o icon-check"></i></li>
         <li class="list-group-item disabled">Almuerzos <i class="fa fa-times-circle-o icon-null"></i></li>
-        <li class="list-group-item disabled">Transporte <i class="fa fa-times-circle-o icon-null"></i></li>
         <li class="list-group-item disabled">Hospedaje <i class="fa fa-times-circle-o icon-null"></i></li>
+        <li class="list-group-item disabled">Transporte <i class="fa fa-times-circle-o icon-null"></i></li>
       </ul>
     </div>
   </div>
 
   <!-- Paquete Parcial -->
   <div class="card" data-type="B" data-payment-type-id="191" data-payment-commission-amount="0" data-amount="40" data-amount-cr="23000" data-description="Paquete parcial ECSL 2019">
-    <h3 class="card-header text-center">Parcial</h3>
+    <h5 class="card-header text-center">Parcial</h5>
     <div class="card-body m-0 px-0 pb-0">
-      <h2 class="text-center font-weight-bold">US$ 50.00</h2>
+      <h3 class="text-center font-weight-bold">US$ 50.00</h3>
       <div class="font-italic text-center mb-4">Incluye:</div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">* Ingreso al evento <i class="fa fa-check-circle-o icon-check"></i></li>
         <li class="list-group-item">Promocional completo <i class="fa fa-check-circle-o icon-check"></i></li>
         <li class="list-group-item">Diploma de participación <i class="fa fa-check-circle-o icon-check"></i></li>
         <li class="list-group-item">** Almuerzos <i class="fa fa-check-circle-o icon-check"></i></li>
-        <li class="list-group-item disabled">Transporte <i class="fa fa-times-circle-o icon-null"></i></li>
+        <li class="list-group-item">*** Transporte <i class="fa fa-check-circle-o icon-check"></i></li>
         <li class="list-group-item disabled">Hospedaje <i class="fa fa-times-circle-o icon-null"></i></li>
       </ul>
     </div>
@@ -36,9 +41,9 @@
 
   <!-- Paquete Semi Completo -->
   <div class="card" data-type="C" data-payment-type-id="192" data-payment-commission-amount="0" data-amount="125" data-amount-cr="86000" data-description="Paquete semi completo ECSL 2019">
-    <h3 class="card-header text-center">Semi completo</h3>
+    <h5 class="card-header text-center">Semi completo</h5>
     <div class="card-body m-0 px-0 pb-0">
-      <h2 class="text-center font-weight-bold">US$ 125.00</h2>
+      <h3 class="text-center font-weight-bold">US$ 125.00</h3>
       <div class="font-italic text-center mb-4">Incluye:</div>
       <ul class="list-group list-group-flush" >
         <li class="list-group-item">* Ingreso al evento <i class="fa fa-check-circle-o icon-check"></i></li>
@@ -53,9 +58,9 @@
 
   <!-- Paquete Completo -->
   <div class="card" data-type="D" data-payment-type-id="193" data-payment-commission-amount="0" data-amount="150" data-amount-cr="86000" data-description="Paquete completo ECSL 2019">
-    <h3 class="card-header text-center">Completo</h3>
+    <h5 class="card-header text-center">Completo</h5>
     <div class="card-body m-0 px-0 pb-0">
-      <h2 class="text-center font-weight-bold">US$ 150.00</h2>
+      <h3 class="text-center font-weight-bold">US$ 150.00</h3>
       <div class="font-italic text-center mb-4">Incluye:</div>
       <ul class="list-group list-group-flush" >
         <li class="list-group-item">* Ingreso al evento <i class="fa fa-check-circle-o icon-check"></i></li>
